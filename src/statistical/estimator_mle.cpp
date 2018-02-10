d993935ad5599158912c6f2d83ea568316f91c8b
Federico Reghenzani <federico.reghenzani@polimi.it>
2018-02-10 18:24:30 +0100

    [Estimator_MLE] Fix initial guess on variance
    An initial guess of 1 is not suitable for measurements in the order of
    10^6. However, a guess for the sigma < 1 is not suitable for any
    estimation.
    
    Signed-off-by: Federico Reghenzani <federico.reghenzani@polimi.it>

--- src/statistical/estimator_mle.cpp
+++ src/statistical/estimator_mle.cpp
@@ -131,7 +131,7 @@ void GEV_Function<T_INPUT, T_TIME>::accumulate_gradient_term(const double* param
 template <typename T_INPUT, typename T_TIME>
 bool Estimator_MLE<T_INPUT, T_TIME>::run(const MeasuresPool<T_INPUT, T_TIME> &measures) noexcept {
 
-	double parameters[3] = {0., 1., 0.};
+	double parameters[3] = {0., measures.max()/100 > 1 ? measures.max()/100 : 1., 0.};
 	ceres::GradientProblemSolver::Options options;
 
 	// TODO reliability considerations
@@ -173,4 +173,5 @@ bool Estimator_MLE<T_INPUT, T_TIME>::run(const MeasuresPool<T_INPUT, T_TIME> &me
 }
 
 template class Estimator_MLE<int, double>;
+template class Estimator_MLE<unsigned int, unsigned long>;
 
