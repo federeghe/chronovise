43e4f002bdf075ca9e7525a5beebbe4f6a4b164d
Federico Reghenzani <federico.reghenzani@polimi.it>
2018-02-11 21:35:16 +0100

    [InputGenerator] Add documentation
    Signed-off-by: Federico Reghenzani <federico.reghenzani@polimi.it>

--- include/input/generator.hpp
+++ include/input/generator.hpp
@@ -1,12 +1,42 @@
+/*
+ *  chronovise - Copyright 2018 Politecnico di Milano
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file generator.hpp
+ * @author Check commit authors
+ * @brief File containing the InputGenerator abstract base class.
+ */
+
 #ifndef INPUT_GENERATOR_HPP_
 #define INPUT_GENERATOR_HPP_
 
 namespace chronovise {
 
+/**
+ * The InputGenerator base class. It is in charge of generating inputs
+ * that should be passed to program that is being measuring.
+ */
 template <typename T>
 class InputGenerator {
 
 public:
+
+	/**
+	 * Generates a new input value.
+	 */
 	virtual T get() noexcept = 0;
 
 };
--- include/input/generator_uniform.hpp
+++ include/input/generator_uniform.hpp
@@ -1,20 +1,56 @@
+/*
+ *  chronovise - Copyright 2018 Politecnico di Milano
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file generator_uniform.hpp
+ * @author Check commit authors
+ * @brief File containing the UniformInputGenerator class.
+ */
+
 #include "input/generator.hpp"
 
 #include <random>
 
 namespace chronovise {
 
+/**
+ * An InputGenerator implementation that provides uniformly distributed
+ * numbers of generic type T.
+ */
 template <typename T>
 class UniformInputGenerator : public InputGenerator<T> {
 
 public:
 
+	/**
+	 * Min-Max constructor. It inizializes the seed from
+	 * a random available device. The numbes are drawn from
+	 * the interval [min,max] with a uniform probability.
+	 * @param min The maximum value of the generated values
+	 * @param max The maximum value of the generated values
+	 */
 	UniformInputGenerator(T min, T max) : mt(rd())
 	{
 		this->min = min;
 		this->max = max;
 	} 
 
+	/**
+	 * @copydoc InputGenerator::get()
+	 */
 	virtual T get() noexcept override;
 
 private:
