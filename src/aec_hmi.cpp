dd8c2320168b417890c36496fc96e7245ee1d6c1
Federico Reghenzani <federico.dev@reghe.net>
2018-02-15 23:32:20 +0100

    [HMI] Minor change in data visualization
    Signed-off-by: Federico Reghenzani <federico.dev@reghe.net>

--- src/aec_hmi.cpp
+++ src/aec_hmi.cpp
@@ -2,21 +2,45 @@
 #include "global.hpp"
 #include <iostream>
 #include <iomanip>
+#include <string>
 
 namespace chronovise {
+
+static void print_title(const std::string &title) {
+
+	std::cerr.width(80);
+	std::cerr.fill('-');
+	std::cerr << std::left << "+" << std::right << "+" << std::endl;
+
+	std::cerr << std::left << "| ";
+	std::cerr.width(80-2);
+	std::cerr.fill(' ');
+	std::cerr << std::left << title << std::right << "|" << std::endl;
+
+	std::cerr.width(80);
+	std::cerr.fill('-');
+	std::cerr << std::left << "+" << std::right << "+" << std::endl;
+
+	std::cerr.fill(' ');
+}
 
 template <typename T_INPUT, typename T_TIME>
 void AbstractExecutionContext<T_INPUT,T_TIME>::print_distributions_summary() const noexcept {
 
 	std::cerr << std::endl;
-	std::cerr << "The following EV distributions have been estimated:" << std::endl;
+	print_title("Distributions summary");
+
+	std::cerr << "| Legend: G - Gumbell, W - Weibull, F - Frechet" << std::endl;
 
 	size_t i=0;
 	for (auto it=ev_dist_estimated.cbegin(); it != ev_dist_estimated.cend(); it++, i++) {
-		std::cerr << std::setw(4) << '#' << i << ": "
-			  << "location=" << std::setw(16) << it->get_location() 
-			  << "\tscale=" << std::setw(16) << it->get_scale() 
-			  << "\tshape=" << std::setw(16) << it->get_shape() << std::endl; 
+		std::cerr << '#'  << std::setw(4) << i << ": "
+			  << "location=" << std::setw(12) << it->get_location()
+			  << "  scale=" << std::setw(12) << it->get_scale()
+			  << "  shape=" << std::setw(12) << it->get_shape() << " ["
+			  << (it->is_gumbell() ? 'G' : it->is_frechet() ? 'F' : 'W') << "]";
+
+		std::cerr << std::endl;
 	}
 	std::cerr << std::endl;
 
--- src/utility/utility.cpp
+++ src/utility/utility.cpp
@@ -9,13 +9,11 @@ namespace utility {
 
 void print_welcome_message() noexcept {
 
-	std::cerr <<
-	"+-------------------------------------------------------------------+" << std::endl <<
+	std::cerr << std::endl <<
 	"| " CFG_FRAMEWORK_NAME  << std::endl <<
 	"| Version: " CFG_FRAMEWORK_VERSION << std::endl <<
 	"| GIT rev: " CFG_FRAMEWORK_GIT_VERSION << std::endl <<
-	"+-------------------------------------------------------------------+" << std::endl << std::endl;
-
+	std::endl;
 
 }
 
