5343be897a92301ea080da8b064327509382c647
Federico Reghenzani <federico.reghenzani@polimi.it>
2018-03-01 11:16:06 +0100

    [Statistical/TestAD] First working implementation
    Signed-off-by: Federico Reghenzani <federico.reghenzani@polimi.it>

--- src/statistical/test_ad.cpp
+++ src/statistical/test_ad.cpp
@@ -6,14 +6,22 @@
 #include <cmath>
 #include <limits>
 #include <vector>
+#include <random>
 
 namespace chronovise {
 
 /** @private */
 namespace local_test_ad {
 
-	template<typename T_INPUT, typename T_TIME>
-	static inline double get_ad_statistic_upper(std::shared_ptr<Distribution> evd, const MeasuresPool<T_INPUT, T_TIME> &measures) noexcept {
+/*
+   *******************************************************************
+   ******************** TEST STATISTIC CALCULATION *******************
+   *******************************************************************
+*/
+
+	template<typename T_TIME>
+	static inline double get_ad_statistic_upper(std::shared_ptr<Distribution> evd,
+						const std::vector<T_TIME> &measures) noexcept {
 
 		size_t cardinality = measures.size();
 		double S1=0.0, S2=0.0;
@@ -35,8 +43,9 @@ namespace local_test_ad {
 		return (double)cardinality / 2 - S1 - S2;
 	}
 
-	template<typename T_INPUT, typename T_TIME>
-	static inline double get_ad_statistic_lower(std::shared_ptr<Distribution> evd, const MeasuresPool<T_INPUT, T_TIME> &measures) noexcept {
+	template<typename T_TIME>
+	static inline double get_ad_statistic_lower(std::shared_ptr<Distribution> evd, 
+						const std::vector<T_TIME> &measures) noexcept {
 
 		size_t cardinality = measures.size();
 		double S1=0.0, S2=0.0;
@@ -59,21 +68,64 @@ namespace local_test_ad {
 		return - 3 * (double)cardinality / 2 + S1 - S2;
 	}
 
-	template<typename T_INPUT, typename T_TIME>
-	static inline double get_ad_statistic(std::shared_ptr<Distribution> evd, const MeasuresPool<T_INPUT, T_TIME> &measures) noexcept {
+	template<typename T_TIME>
+	static inline double get_ad_statistic(std::shared_ptr<Distribution> evd, const std::vector<T_TIME> &measures) noexcept {
 		double upper = get_ad_statistic_upper(evd, measures);
 		double lower = get_ad_statistic_lower(evd, measures);
 
 		return upper + lower;
 	}
 
+/*
+   *******************************************************************
+   ******************** CRITICAL VALUE CALCULATION *******************
+   *******************************************************************
+*/
+
+	static inline double get_ad_critical_value(std::shared_ptr<Distribution> evd, bool MAD,
+				size_t sample_cardinality, double significance_level) noexcept {
+
+		constexpr unsigned int nr_runs = 1000U;
+
+		std::vector<double> crits;
+		crits.resize(nr_runs);
+
+		std::vector<double> sample;
+		sample.reserve(sample_cardinality);
+
+		std::mt19937 random_gen(std::random_device{}());
+		std::uniform_real_distribution<double> distribution(0.0,1.0);
+
+		for (unsigned long i=0; i < nr_runs; i++) {
+
+			for (unsigned int j=0; j < sample_cardinality; j++) {
+				// Inverse transform sampling
+				sample.push_back(evd->quantile(distribution(random_gen)));
+			}
+
+			std::sort(sample.begin(), sample.end());
+
+			if (MAD) {
+				crits[i] = get_ad_statistic_upper(evd, sample);
+			} else {
+				crits[i] = get_ad_statistic(evd, sample);
+			}
+		}
+
+		std::sort(crits.begin(),crits.end());
+
+
+		return crits[(int)(significance_level * nr_runs)];
+	}	
 
 }
 
 template <typename T_INPUT, typename T_TIME>
 void TestAD<T_INPUT, T_TIME>::run(const MeasuresPool<T_INPUT, T_TIME> &measures) {
 
-	if(measures.size() < get_minimal_sample_size()) {
+	const size_t size = measures.size();
+
+	if(size < get_minimal_sample_size()) {
 		throw std::invalid_argument("The number of samples is too low for this test.");
 	}
 
@@ -86,10 +138,12 @@ void TestAD<T_INPUT, T_TIME>::run(const MeasuresPool<T_INPUT, T_TIME> &measures)
 	using namespace local_test_ad;
 
 	// TODO
-	double ad_critical_value = (1. + safe_margin); // * AndersonDarlingCV::get_critical_value(config::sample_cardinality, evt_param);
+	double ad_critical_value = (1. + safe_margin) *
+					get_ad_critical_value(this->ref_distribution,
+								MAD, size, this->significance_level);
 
 	// Using MeasuresPool, the [] operator guarantees ordering.
-	double statistics = get_ad_statistic(this->ref_distribution, measures);
+	double statistics = get_ad_statistic<T_TIME>(this->ref_distribution, measures.get_ordered_vector());
 
 	if (statistics > ad_critical_value) {
 		this->reject = true;
