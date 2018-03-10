d10f3a06d556ce63663ec1d2029b52cb9cae275d
Federico Reghenzani <federico.reghenzani@polimi.it>
2018-03-10 13:24:10 +0100

    [Test] Add PoT test
    Signed-off-by: Federico Reghenzani <federico.reghenzani@polimi.it>

---
+++ tests/test_pot.cpp
@@ -0,0 +1,63 @@
+#include "gtest/gtest.h"
+#include "evt/evtapproach_pot.hpp"
+
+using namespace chronovise;
+
+#include <cmath>
+
+// The fixture for testing class Project1. From google test primer.
+class EVPoT_Test : public ::testing::Test {
+protected:
+
+
+};
+
+TEST_F(EVPoT_Test, PoTCheck) {
+
+	MeasuresPool<int, unsigned long> source;
+
+	for (int i=1; i<=100; i++) {
+		source.push(1,i);
+	}
+	for (int i=1; i<=100; i++) {
+		source.push(1,i);
+	}
+	
+	EVTApproach_PoT<int, unsigned long> ebm(50);
+	ebm.perform(MeasuresPoolSet<int, unsigned long>(source, 0.5));
+
+	auto &result = ebm.get_training_pool();
+
+	ASSERT_EQ(result.size(), 50);
+
+	int i=51;
+	for (auto it = result.cbegin(); it != result.cend(); it++, i++) {
+		ASSERT_EQ(it->second, i);
+	}
+
+}
+
+TEST_F(EVPoT_Test, PoTCheck_Testset) {
+	MeasuresPool<int, unsigned long> source;
+
+	for (int i=1; i<=100; i++) {
+		source.push(1,i);
+	}
+	for (int i=1; i<=100; i++) {
+		source.push(1,i);
+	}
+	
+	EVTApproach_PoT<int, unsigned long> ebm(50);
+	ebm.perform(MeasuresPoolSet<int, unsigned long>(source, 0.5));
+
+	auto &result = ebm.get_test_pool();
+
+	ASSERT_EQ(result.size(), 50);
+
+	int i=51;
+	for (auto it = result.cbegin(); it != result.cend(); it++, i++) {
+		ASSERT_EQ(it->second, i);
+	}
+
+
+}
