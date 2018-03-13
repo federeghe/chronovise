125c59d9dd3deb4e6fddbeedd6de6e9bf540c344
Federico Reghenzani <federico.reghenzani@polimi.it>
2018-03-13 20:52:48 +0100

    [Statistical] Add PWM estimator for GPD
    Signed-off-by: Federico Reghenzani <federico.reghenzani@polimi.it>

---
+++ include/statistical/estimator_pwm.hpp
@@ -0,0 +1,86 @@
+/*
+ *  chronovise - Copyright 2018 Politecnico di Milano
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file estimator_pwm.hpp
+ * @author Check commit authors
+ * @brief File containing the Estimator_PWM class
+ */
+
+#ifndef STATISTICAL_ESTIMATOR_PWM_HPP_
+#define STATISTICAL_ESTIMATOR_PWM_HPP_
+
+#include "statistical/estimator.hpp"
+#include "statistical/distribution.hpp"
+
+#include <memory>
+
+namespace chronovise {
+
+/**
+ * The Probability Weighted Moment estimator class.
+ */
+template <typename T_INPUT, typename T_TIME=unsigned long>
+class Estimator_PWM : public Estimator<T_INPUT, T_TIME> {
+
+public:
+#warning
+// QUANTILE ESTIMATION FOR THE GENERALIZED
+// PARETO DISTRIBUTION WITH APPLICATION TO
+// FINANCE 
+    /**
+     * A simple destructor
+     */
+    virtual ~Estimator_PWM() {}
+
+    /**
+     * @copydoc Estimator::run()
+     */
+    virtual bool run(const MeasuresPool<T_INPUT, T_TIME> &measures) override;
+
+    /**
+     * @copydoc Estimator::get_result()
+     */
+    virtual std::shared_ptr<Distribution> get_result() const override final {
+        if (result == nullptr)
+            throw std::runtime_error("Estimator never run");
+        
+        return result;
+    }
+
+    /**
+     * @copydoc Estimator::get_status()
+     */
+    virtual estimator_status_t get_status() const noexcept {
+        return status;
+    }
+
+    /**
+     * @copydoc Estimator::get_minimal_sample_size()
+     */
+    virtual unsigned long get_minimal_sample_size() const noexcept {
+        return 10;
+    }
+
+private:
+    std::shared_ptr<Distribution> result;
+    estimator_status_t status=estimator_status_t::UNKNOWN;
+
+};
+
+} // namespace chronovise
+
+#endif // STATISTICAL_ESTIMATOR_PWM_HPP_
---
+++ src/statistical/estimator_pwm.cpp
@@ -0,0 +1,39 @@
+#include "statistical/estimator_pwm.hpp"
+
+#include "evt/gpd_distribution.hpp"
+#include "global.hpp"
+
+namespace chronovise {
+
+template <typename T_INPUT, typename T_TIME>
+bool Estimator_PWM<T_INPUT, T_TIME>::run(const MeasuresPool<T_INPUT, T_TIME> &measures) {
+
+    const size_t n = measures.size();
+    const auto mu = measures.min();
+
+    T_TIME W0 = 0;
+    T_TIME W1 = 0;
+
+
+    for (size_t i=0; i < n; i++) {
+        auto curr = measures[i] - mu;
+        W0 += curr;
+        W1 += (1. - (static_cast<double>(i) - 0.35) / n) * curr;
+    }
+
+    W0 /= n;
+    W1 /= n;
+
+    T_TIME xi = W0 / (W0 - 2. * W1) - 2.; 
+    T_TIME sg = 2. * W0 * W1 / (W0 - 2. * W1);
+
+    result = std::make_shared<GPD_Distribution>(measures.min(), sg, xi);
+
+    status = estimator_status_t::SUCCESS;
+
+    return true;
+}
+
+TEMPLATE_CLASS_IMPLEMENTATION(Estimator_PWM);
+
+} // namespace chronovise
