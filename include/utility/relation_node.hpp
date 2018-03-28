b93b67ff2507082e960a0662937f5bace1b28270
Federico Reghenzani <federico.reghenzani@polimi.it>
2018-03-28 16:17:06 +0200

    [Utility] Add RelationNode class and related test
    Signed-off-by: Federico Reghenzani <federico.reghenzani@polimi.it>

---
+++ include/utility/relation_node.hpp
@@ -0,0 +1,80 @@
+/*
+ *  chronovise - Copyright 2018 Politecnico di Milano
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file utility/relation_node.hpp
+ * @author Check commit authors
+ * @brief File containing the RelationNode and related classes/types
+ */
+
+#ifndef UTILITY_RELATION_NODE_HPP_
+#define UTILITY_RELATION_NODE_HPP_
+
+#include <list>
+#include <memory>
+
+namespace chronovise {
+
+typedef enum class relation_node_e {
+
+    UKNOWN,  /** Error value, it should not be used */
+    TEST,    /** Statistical test */
+    OPERATOR,/** Operator */
+
+} relation_node_t;
+
+class RelationNode {
+
+public:
+    RelationNode(relation_node_t type) : type(type)
+    {
+    }
+
+    virtual ~RelationNode()
+    {
+    }
+
+    relation_node_t get_type() const noexcept { return this->type; }
+
+    void add_child(std::unique_ptr<RelationNode> &rn) {
+        if (this->type == relation_node_t::TEST) {
+            throw std::invalid_argument("A test cannot have children.");
+        }
+        this->children.push_back(std::move(rn));
+    }
+
+    std::list<std::unique_ptr<RelationNode>>::const_iterator cbegin() const noexcept {
+        return this->children.cbegin();
+    }
+
+    std::list<std::unique_ptr<RelationNode>>::const_iterator cend() const noexcept {
+        return this->children.cend();
+    }
+
+    const std::list<std::unique_ptr<RelationNode>> & get_children() const noexcept {
+        return this->children;
+    }
+
+private:
+    relation_node_t type;
+
+    std::list<std::unique_ptr<RelationNode>> children;
+};
+
+} // namespace chronovise
+
+
+#endif
---
+++ tests/test_relations.cpp
@@ -0,0 +1,50 @@
+#include "gtest/gtest.h"
+#include "utility/relation_node.hpp"
+
+using namespace chronovise;
+
+// The fixture for testing class Project1. From google test primer.
+class Relations_Test : public ::testing::Test {
+protected:
+
+
+};
+
+TEST_F(Relations_Test, TypePersistence) {
+
+    RelationNode relnode(relation_node_t::TEST);
+
+    EXPECT_EQ(relation_node_t::TEST, relnode.get_type());
+
+    RelationNode relnode2(relation_node_t::OPERATOR);
+
+    EXPECT_EQ(relation_node_t::OPERATOR, relnode2.get_type());
+
+}
+
+TEST_F(Relations_Test, Children) {
+
+    std::unique_ptr<RelationNode> child1(new RelationNode(relation_node_t::TEST));
+    std::unique_ptr<RelationNode> child2(new RelationNode(relation_node_t::TEST));
+    std::unique_ptr<RelationNode> child3(new RelationNode(relation_node_t::TEST));
+
+    std::unique_ptr<RelationNode> interm(new RelationNode(relation_node_t::OPERATOR));
+    std::unique_ptr<RelationNode> root (new RelationNode(relation_node_t::OPERATOR));
+
+    interm->add_child(child1);
+    interm->add_child(child2);
+
+    root->add_child(child3);
+    root->add_child(interm);
+
+    EXPECT_EQ(relation_node_t::TEST, (*root->cbegin())->get_type());
+
+}
+
+TEST_F(Relations_Test, Exceptions) {
+
+    std::unique_ptr<RelationNode> child1(new RelationNode(relation_node_t::TEST));
+
+    EXPECT_THROW(child1->add_child(child1) ,std::invalid_argument);
+
+}
