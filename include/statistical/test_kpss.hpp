d72e38a65bfd1b7afb47c7cd9502639f692b2182
Federico Reghenzani <federico@reghe.net>
2019-10-23 13:58:32 -0400

    [Statistical] Add KPSS test (at present not correctly working)
    Signed-off-by: Federico Reghenzani <federico@reghe.net>

---
+++ include/statistical/test_kpss.hpp
@@ -0,0 +1,119 @@
+/*
+ *  chronovise - Copyright 2018 Politecnico di Milano
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file test_kpss.hpp
+ * @author Check commit authors
+ * @brief File containing the KPSS test.
+ */
+
+#ifndef STATISTICAL_TEST_KPSS_HPP_
+#define STATISTICAL_TEST_KPSS_HPP_
+
+#include "statistical/test.hpp"
+#include "statistical/distribution.hpp"
+#include "evt/gev_distribution.hpp"
+#include "global.hpp"
+
+#include <cmath>
+#include <list>
+
+#define TEST_KPSS_MIN_SAMPLES 10
+
+namespace chronovise {
+
+typedef enum class test_kpss_trend_e {
+    LEVEL,
+    TREND
+} test_kpss_trend_t;
+
+/**
+ * The Kwiatkowski–Phillips–Schmidt–Shin test class.
+ */
+template <typename T_INPUT, typename T_TIME=unsigned long>
+class TestKPSS : public StatisticalTest<T_INPUT, T_TIME> {
+
+public:
+
+    /**
+     * The TestKPSS constructor.
+     * @param significance_level The significance level for the test (alpha)
+     * @param n_lags The number of lags of the Ljung-Box test. In non-seasonal data
+     *               it should be around the value of 10.
+     */
+    TestKPSS(double significance_level, unsigned int n_lags, test_kpss_trend_t trend_class = test_kpss_trend_t::LEVEL)
+    : StatisticalTest<T_INPUT,T_TIME>(significance_level), n_lags(n_lags), trend_class(trend_class)
+    {
+    }
+
+    /**
+     * @copydoc StatisticalTest::run()
+     */
+    virtual void run(const MeasuresPool<T_INPUT, T_TIME> &measures) override;
+
+    /**
+     * @copydoc StatisticalTest::get_power()
+     */
+     virtual double get_power() const override {
+        throw std::runtime_error("No power available for this test");
+    }
+
+    /**
+     * @copydoc StatisticalTest::has_power()
+     */
+     virtual bool has_power() const noexcept override {
+        return false;
+    }
+
+    /**
+     * @copydoc StatisticalTest::has_safe_power()
+     */
+     virtual bool has_safe_power() const noexcept override {
+        return false;
+    }
+
+    /**
+     * @copydoc StatisticalTest::get_minimal_sample_size()
+     */
+    virtual unsigned long get_minimal_sample_size() const noexcept override {
+        return TEST_KPSS_MIN_SAMPLES;
+    }
+
+    /**
+     * @copydoc StatisticalTest::get_minimal_sample_size(unsigned short) const
+     */
+    virtual unsigned long get_minimal_sample_size(unsigned short req_power) const override {
+        UNUSED(req_power);
+        throw std::runtime_error("No power available for this test");
+    }
+
+private:
+    unsigned int n_lags;
+    test_kpss_trend_t trend_class;
+
+    double compute_statistics(const MeasuresPool<T_INPUT, T_TIME> &measures) noexcept;
+    double compute_cv_value(const MeasuresPool<T_INPUT, T_TIME> &measures) noexcept;
+
+    static const std::map<test_kpss_trend_t,std::map<float,std::map<int,std::vector<double>>>> coeff_kpss;
+
+};
+
+
+
+} // namespace chronovise
+
+#endif // STATISTICAL_TEST_KPSS_HPP_
+
---
+++ tests/test_test_kpss.cpp
@@ -0,0 +1,129 @@
+#include "gtest/gtest.h"
+#include "statistical/test_kpss.hpp"
+#include "statistical/distribution_uniform.hpp"
+#include "global.hpp"
+
+using namespace chronovise;
+
+#include <cmath>
+
+// The fixture for testing class Project1. From google test primer.
+class Test_KPSS_Test : public ::testing::Test {
+protected:
+
+
+};
+
+TEST_F(Test_KPSS_Test, GetterSetter) {
+    TestKPSS<unsigned long, unsigned long> jb(0.01, 10);
+
+    EXPECT_EQ(0.01, jb.get_significance_level());
+    EXPECT_EQ(false, jb.is_reject());
+    EXPECT_EQ(false, jb.has_power());
+
+}
+
+TEST_F(Test_KPSS_Test, DistributionACCEPT) {
+
+    TestKPSS<unsigned int, double> jb(0.05, 5);
+
+    double norm_ind_values[100] = {
+1.8339,-2.2588,0.8622,0.3188,-1.3077,-0.4336,0.3426,3.5784,2.7694,-1.3499,3.0349,0.7254
+,-0.0631,0.7147,-0.2050,-0.1241,1.4897,1.4090,1.4172,0.6715,-1.2075,0.7172,1.6302,0.4889
+,1.0347,0.7269,-0.3034,0.2939,-0.7873,0.8884,-1.1471,-1.0689,-0.8095,-2.9443,1.4384,0.3252
+,-0.7549,1.3703,-1.7115,-0.1022,-0.2414,0.3192,0.3129,-0.8649,-0.0301,-0.1649,0.6277,1.0933
+,1.1093,-0.8637,0.0774,-1.2141,-1.1135,-0.0068,1.5326,-0.7697,0.3714,-0.2256,1.1174,-1.0891
+,0.0326,0.5525,1.1006,1.5442,0.0859,-1.4916,-0.7423,-1.0616,2.3505,-0.6156,0.7481,-0.1924
+,0.8886,-0.7648,-1.4023,-1.4224,0.4882,-0.1774,-0.1961,1.4193,0.2916,0.1978,1.5877,-0.8045
+,0.6966,0.8351,-0.2437,0.2157,-1.1658,-1.1480,0.1049,0.7223,2.5855,-0.6669,0.1873,-0.0825
+,-1.9330,-0.4390,-1.7947,0.8404
+    };
+
+    MeasuresPool<unsigned int, double> mp;
+    for (int i=0; i<100; i++) {
+        mp.push(i, norm_ind_values[i]);
+    }
+    
+    jb.run(mp);
+
+    EXPECT_FALSE(jb.is_reject());
+
+}
+
+TEST_F(Test_KPSS_Test, DistributionREJECT_1) {
+
+    TestKPSS<unsigned int, double> jb(0.05, 0);
+
+    double start = 1.0;
+
+    MeasuresPool<unsigned int, double> mp;
+    for (int i=0; i<100; i++) {
+        mp.push(i, start/(i+1));
+    }
+    
+    jb.run(mp);
+
+    EXPECT_TRUE(jb.is_reject());
+
+}
+
+TEST_F(Test_KPSS_Test, DistributionREJECT_2) {
+
+    TestKPSS<unsigned int, double> jb(0.05, 2);
+
+    double start = 1.0;
+
+    MeasuresPool<unsigned int, double> mp;
+    double a_2 = 0;
+    double a_1 = 0;
+
+    for (int i=0; i<100; i++) {
+        double a = start/(i+1);
+        mp.push(i, a + a_1 + a_2);
+        a_2 = a_1;
+        a_1 = a;
+    }
+    
+    jb.run(mp);
+
+    EXPECT_TRUE(jb.is_reject());
+
+}
+
+TEST_F(Test_KPSS_Test, DistributionREJECT_3) {
+
+    TestKPSS<unsigned int, double> jb(0.05, 10);
+
+    MeasuresPool<unsigned int, double> mp1;
+    MeasuresPool<unsigned int, double> mp2;
+
+    double norm_ind_values[100] = {
+1.8339,-2.2588,0.8622,0.3188,-1.3077,-0.4336,0.3426,3.5784,2.7694,-1.3499,3.0349,0.7254
+,-0.0631,0.7147,-0.2050,-0.1241,1.4897,1.4090,1.4172,0.6715,-1.2075,0.7172,1.6302,0.4889
+,1.0347,0.7269,-0.3034,0.2939,-0.7873,0.8884,-1.1471,-1.0689,-0.8095,-2.9443,1.4384,0.3252
+,-0.7549,1.3703,-1.7115,-0.1022,-0.2414,0.3192,0.3129,-0.8649,-0.0301,-0.1649,0.6277,1.0933
+,1.1093,-0.8637,0.0774,-1.2141,-1.1135,-0.0068,1.5326,-0.7697,0.3714,-0.2256,1.1174,-1.0891
+,0.0326,0.5525,1.1006,1.5442,0.0859,-1.4916,-0.7423,-1.0616,2.3505,-0.6156,0.7481,-0.1924
+,0.8886,-0.7648,-1.4023,-1.4224,0.4882,-0.1774,-0.1961,1.4193,0.2916,0.1978,1.5877,-0.8045
+,0.6966,0.8351,-0.2437,0.2157,-1.1658,-1.1480,0.1049,0.7223,2.5855,-0.6669,0.1873,-0.0825
+,-1.9330,-0.4390,-1.7947,0.8404
+    };
+
+    for (int i=0; i<100; i++) {
+        double old = 0;
+        if (i >= 10) {
+            auto vi = mp1.cbegin();
+            std::advance(vi, i - 10);
+            old = vi->second;
+        }
+        mp1.push(i, norm_ind_values[i] + old);
+        mp2.push(i, norm_ind_values[i]);
+    }
+
+    jb.run(mp1);
+    EXPECT_TRUE(jb.is_reject());
+    jb.run(mp2);
+    EXPECT_FALSE(jb.is_reject());
+
+}
+
