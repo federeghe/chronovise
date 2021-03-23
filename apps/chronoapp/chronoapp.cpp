5c5fac329b143f3e62ebbc8523d7e205835dd4cb
Federico Reghenzani <federico@reghe.net>
2021-03-23 17:29:08 +0100

    [Apps] Chronoapp help message

--- apps/chronoapp/chronoapp.cpp
+++ apps/chronoapp/chronoapp.cpp
@@ -32,7 +32,9 @@ bool parse_parameters(int argc, const char *argv[]) {
     for (int i=1; i<argc; i++) {
         std::string curr_arg(argv[i]);
         
-        if (curr_arg == "--describe-csv") {
+        if (curr_arg == "--help") {
+            return true;
+        } else if (curr_arg == "--describe-csv") {
             describe_csv = true;
         } else if (curr_arg == "--csv-output") {
             csv_output = true;
